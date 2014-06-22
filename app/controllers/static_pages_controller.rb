f4710f5aab384b8f98cd1ba65091e92814b09cae
Panagiotis Liakos <liakospanagiotis@gmail.com>
2014-06-22 11:11:44 +0300

    fixed footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,14 @@ class StaticPagesController < ApplicationController
 
   def home
     if signed_in?
+      if sce?
+        @fleets = current_user.shipping_company.fleets.paginate(page: params[:page])
+        @ships = current_user.shipping_company.ships.paginate(page: params[:page])
+        @voyages = current_user.shipping_company.voyages.paginate(page: params[:page])
+      elsif ase?
+        @fleets = Fleet.none
+        @voyages_ports = current_user.agent.voyages_ports.paginate(page: params[:page])
+      end
     end
   end
   
