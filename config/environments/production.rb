6fff9a677031ac426628ea6cb3e8b492a0f85f96
Panagiotis Liakos <liakospanagiotis@gmail.com>
2014-07-09 18:45:57 +0300

    adding e-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #setup gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'poseidonxxviii',
+    password:             'poseidon1!',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  
 end
