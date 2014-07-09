caeea324b3e67969f35ded8c16092e5e58bdde42
Panagiotis Liakos <liakospanagiotis@gmail.com>
2014-07-09 18:40:39 +0300

    adding e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,16 @@ SampleApp::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => "dev.poseidon.com" }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'poseidonxxviii',
+    password:             'poseidon1!',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
